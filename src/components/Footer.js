75d321a7ab4372a4b521b00674c8bc40e89601e7
Wlla Talafha <wlla.issa396@gmail.com>
2022-08-08 21:20:13 +0300

    done

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Footer extends Component {
+    render() {
+      return (
+        <div className="Footer">
+            <p>all rights are reserved by wlla</p>
+        </div>   
+        
+      )
+    }
+  }
+  
+  export default Footer;
